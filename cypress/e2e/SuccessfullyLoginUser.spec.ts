68482a2157303a0cad2442b2d723ed303c788b65
Greg <grzegorz.domanowski.ais@gmail.com>
2023-01-11 23:55:56 +0100

    test: update login test

--- cypress/e2e/SuccessfullyLoginUser.spec.ts
+++ cypress/e2e/SuccessfullyLoginUser.spec.ts
@@ -1,11 +1,8 @@
-import { login } from '../pages/login'
+import { LoginToPage } from '../flows/Login.flow'
 
 // This test will check if user can successfully log in to the page
 describe('Functional test, successfully login user', () => {
   it('should login user', () => {
-    cy.visit('/')
-    login.loginInput_type(Cypress.env('login'))
-    login.passwordInput_type(Cypress.env('password'))
-    login.loginButton_click()
+    LoginToPage()
   })
 })
